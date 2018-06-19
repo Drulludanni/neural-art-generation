063453d4bcb3c09293468c441585475c35be10d3
drulludanni <drulludanni5@gmail.com>
2018-06-19 20:19:59 +0000

    massively improved the performance for the data generation and ensured  corrected where the width and height were swapped

--- art_generation.py
+++ art_generation.py
@@ -9,32 +9,31 @@ from PIL import Image
 
 gif_folder = "gif_files"
 
-def generate_input(width=800, height=800, scale=1, offset=0.0):
-    values = []
-    for i in range(width):
-        for j in range(height):
-            x = scale*(i/width - 0.5)
-            y = scale*(j/height - 0.5)
-            value = []
-            # value.append(x)
-            value.append(y)
-            value.append(np.sqrt((x+0.1)**2 + y**2))
-            value.append(offset)
-            # value.append(np.sqrt((x-0.5)**2 + (y-0.5)**2))
-            # value.append(np.sign(y)*np.sqrt(abs(y)))
-            # value.append(np.sign(x)*np.sqrt(abs(x)))
-            # value.append(1/(np.sqrt((x+0.4)**2 + y**2)+1))
-            # value.append(1/(np.sqrt((x-0.4)**2 + y**2)+1))
-            # value.append(np.sqrt((x-0.5)**2 + y**2))
-            # value.append(np.sqrt((x+0.1)**2 + (y+ 0.1)**2))
-            # value.append(np.sqrt((x+offset)**2 + (y)**2))
-            # value.append(np.sqrt((x-offset)**2 + (y)**2))
-            # value.append(np.sqrt((x+0.5)**2 + (y-0.5)**2))
-            # value.append(np.sqrt((x+0.5)**2 + (y-0.5)**2))
-            # value.append(np.sqrt((x-0.5)**2 + (y+0.5)**2))
+def generate_input(width=800, height=800, zoom=1, dt=0.0):
+    """
+    :param width: the width of the picture/frame
+    :param height: the height of the picture/frame
+    :param zoom: how much zoomed out the picture is
+    :param dt:
+    :return:
+    """
+    aspect = height/width
+    Y, X = np.mgrid[0:height,0:width]
+    X = 2*zoom*(np.reshape(X, (height, width, 1))/width - 0.5)
+    Y = 2*zoom*(np.reshape(Y, (height, width, 1))/height - 0.5)*aspect
+    dt = np.zeros(X.shape) + dt
+    values = [
+        X,
+        Y,
+        X+Y+1,
+        X-Y-1,
+        # np.sqrt(X**2 + Y**2),
+        dt
+              ]
+    conc = np.concatenate(values, axis=2)
+    print(np.shape(conc))
+    return conc
 
-            values.append(value)
-    return np.array(values)
 
 def create_model(in_shape):
     inp = Input(shape=in_shape)
@@ -59,26 +58,26 @@ def create_model(in_shape):
     return model
 
 
-def create_video(x=800, y=800, n_frames=100):
-    values = generate_input(x, y, scale=1, offset=0)
+def create_video(x=800, y=800,zoom=1, n_frames=100, dt=0.001 ):
+    # generate the data for the first frame early so that
+    # the model can be built to accept it
+    values = generate_input(x, y, dt=0)
     model = create_model(np.shape(values)[1:])
-    print(model.summary())
     print("predicting")
-    offset = 0.001
     frames = []
-    for i in range(n_frames):
+    for i in range(1, n_frames):
         print(i)
-        values = generate_input(x,y,scale=1, offset=i*offset)
         rgb = model.predict(values, batch_size=400)
-        rgb = np.reshape(rgb,(x,y,3))
+        rgb = np.reshape(rgb,(y,x,3))
         rgb = np.uint8((rgb)*255)
         im = Image.fromarray(rgb)
         file = os.path.join(gif_folder,"out{}.png".format(i))
         im.save(file)
         frames.append(rgb)
+        values = generate_input(x,y,zoom=zoom, dt=(i)*dt)
 
     write_frames_to_video("neural_output.avi", "MJPG", frames, dim=(x,y))
-    write_frames_to_video("neural_output2.avi", "X264", frames, dim=(x,y))
+    # write_frames_to_video("neural_output2.avi", "X264", frames, dim=(x,y))
 
 def write_frames_to_video(name, codec, frames, dim=(800, 800)):
     try:
@@ -94,21 +93,19 @@ def write_frames_to_video(name, codec, frames, dim=(800, 800)):
         print("failed for:", codec)
         print(e)
 
-def create_png(x=800, y=800):
-    values = generate_input(x, y, scale=1, offset=0)
+def create_png(x=800, y=800, zoom=1):
+    values = generate_input(x, y, zoom)
     model = create_model(np.shape(values)[1:])
-    print(model.summary())
     print("predicting")
-    values = generate_input(x, y, scale=1)
     rgb = model.predict(values, batch_size=400)
-    rgb = np.reshape(rgb, (x, y, 3))
+    rgb = np.reshape(rgb, (y, x, 3))
     rgb = np.uint8((rgb) * 255)
     im = Image.fromarray(rgb)
     file = "out.png"
     im.save(file)
 
 if __name__ == '__main__':
-    create_video(n_frames=100)
-
+    #create_video(1000, 500,n_frames=100)
+    create_png(1000, 500, zoom=5)
 
 
